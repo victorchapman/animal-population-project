2fb78a4fd97849e55658e002588af71318e6fe55
Californium98 <65624008+Californium98@users.noreply.github.com>
2021-04-12 22:30:17 +0200

    Got class to work and got form started.

--- src/components/AnimalForm.jsx
+++ src/components/AnimalForm.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AnimalForm = () => {
-  return <div>hello</div>;
+  const [month, setMonth] = useState(0);
+
+  const updateMonths = (e) => {
+    setMonth(e.target.value);
+    console.log(month);
+  };
+
+  const getLitterSize = () => {};
+
+  return (
+    <div>
+      <input onChange={updateMonths} type="text" />
+      <button onClick={getLitterSize}>See Size</button>
+    </div>
+  );
 };
 
 export default AnimalForm;
