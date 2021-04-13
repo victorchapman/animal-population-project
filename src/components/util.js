7b351ddb25a23c5f7bdc620f8b7b7cb7feb9f5fe
Californium98 <65624008+Californium98@users.noreply.github.com>
2021-04-13 08:41:16 +0200

    Created litterSize functions with params.

--- src/components/util.js
+++ src/components/util.js
@@ -1,47 +1,54 @@
-const monthsStartingAge = 4;
-const monthsBeforeFirstLitter = 0;
-const litterSize = 6;
-const months = 18;
+// const monthsStartingAge = 4;
+// const monthsBeforeFirstLitter = 0;
+// const litterSize = 6;
+// const months = 18;
 
-const catArray = [];
+const AnimalArray = [];
 
-class Cat {
-  constructor(age, refrTime) {
-    this.minAge = 4;
-    this.age = age;
-    this.refrTime = refrTime;
-  }
+export const litterSize = ({
+  monthsStartingAge,
+  monthsBeforeFirstLitter,
+  litterSize,
+  months,
+}) => {
+  class Animal {
+    constructor(age, refrTime) {
+      this.minAge = 4;
+      this.age = age;
+      this.refrTime = refrTime;
+    }
 
-  createLitter() {
-    for (let i = 0; i < litterSize; i++) {
-      catArray.push(new Cat(0, 2));
+    createLitter() {
+      for (let i = 0; i < litterSize; i++) {
+        AnimalArray.push(new Animal(1, 2));
+      }
+    }
+    decreaseRefrTime() {
+      if (this.refrTime === 0) {
+        this.refrTime = 3;
+        this.createLitter();
+      } else {
+        this.refrTime -= 1;
+      }
     }
   }
-  decreaseRefrTime() {
-    if (this.refrTime === 0) {
-      this.refrTime = 3;
-      this.createLitter();
-    } else {
-      this.refrTime -= 1;
-    }
+
+  let time = 0;
+
+  const mother = new Animal(monthsStartingAge, monthsBeforeFirstLitter);
+
+  catArray.push(mother);
+
+  while (time < months) {
+    AnimalArray.forEach((animal) => {
+      if (animal.age >= 4) {
+        animal.decreaseRefrTime();
+      }
+      animal.age += 1;
+    });
+    console.log('Month', time + 1);
+    console.log('Number of cats', AnimalArray.length - 1);
+
+    time += 1;
   }
-}
-
-let time = 0;
-
-const mother = new Cat(monthsStartingAge, monthsBeforeFirstLitter);
-
-catArray.push(mother);
-
-while (time < 18) {
-  catArray.forEach((cat) => {
-    if (cat.age >= 4) {
-      cat.decreaseRefrTime();
-    }
-    cat.age += 1;
-  });
-  console.log('Month', time + 1);
-  console.log('Number of cats', catArray.length - 1);
-
-  time += 1;
-}
+};
