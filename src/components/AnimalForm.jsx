118c2308b2e76ddad0902872368923e1c910d3c1
Californium98 <65624008+Californium98@users.noreply.github.com>
2021-04-13 19:48:44 +0200

    Nevin1901 helped get base project working.

--- src/components/AnimalForm.jsx
+++ src/components/AnimalForm.jsx
@@ -4,36 +4,40 @@ import { calcLitterSize2 } from './newUtil';
 const AnimalForm = () => {
   const [litterSize, setLitterSize] = useState(0);
   const [month, setMonth] = useState(0);
-
-  let animalEmojiArray = [];
+  const [animalEmojiArray, setAnimalEmojiArray] = useState([]);
 
   const updateMonths = (e) => {
     setMonth(e.target.value);
     console.log(month);
-    animalEmojiArray = [];
   };
   const getLitterSize = () => {
     const response = calcLitterSize2(4, 4, 2, 6, month);
     console.log(response);
     setLitterSize(response);
-  };
-  useEffect(() => {
     for (let i = 0; i < litterSize; i++) {
-      animalEmojiArray.push('😼');
+      setAnimalEmojiArray((prevAnimalEmojiArray) => [
+        ...prevAnimalEmojiArray,
+        '😺',
+      ]);
     }
     console.log(animalEmojiArray);
-  }, [getLitterSize]);
+  };
 
   return (
     <div>
       <input onChange={updateMonths} type="number" min="1" max="20" />
       <button onClick={getLitterSize}>See Size</button>
       <p>{litterSize}</p>
-      <li>
+      {/* <div>
         {animalEmojiArray.map((cat) => {
-          return <ul>{cat}</ul>;
+          return <span>{cat}</span>;
         })}
-      </li>
+      </div> */}
+      <div>
+        {[...Array(litterSize)].map((element, index) => (
+          <span>😺</span> // ok this should work
+        ))}
+      </div>
     </div>
   );
 };
